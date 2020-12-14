8190ea56613512c78bebd929788b17a9f102e66d
morgom94 <erick.morales@softtekclients.com>
2020-12-14 12:58:42 -0800

    First commit

---
+++ src/main/java/morgom/maven/MagicBuilder.java
@@ -0,0 +1,9 @@
+package morgom.maven;
+
+public class MagicBuilder {
+
+    public static int getLucky() {
+        return 7;
+    }
+
+}
---
+++ src/main/java/morgom/maven/MessageBuilder.java
@@ -0,0 +1,13 @@
+package morgom.maven;
+
+public class MessageBuilder {
+
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+}
